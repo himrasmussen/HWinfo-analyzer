0efced6e62b415191403219fd0a777eaeb4ed468
Christian <himrasmussen@gmail.com>
2017-02-11 11:08:02 +0100

    added encoding to top of file

--- webapp/gpu.py
+++ webapp/gpu.py
@@ -27,7 +27,7 @@ def main(csv_data):
     try:
         data = csv_data[parameter]
     except KeyErro:
-        message += "Gpu temp: Key error\n"
+        message += "Gpu temp: Key error"
     else:
         message += "Gpu temp max: {}\n".format(max(data))
         message += "Gpu temps sample: {}\n".format(random.sample(data, 20))
@@ -35,8 +35,8 @@ def main(csv_data):
     #Average hz
     data = csv_data["GPU Clock [MHz]"]
     try:
-        message += "GPU avg hz: {}\n".format(sum(data) / len(data))
+        message += "GPU avg hz: {}".format(sum(data) / len(data))
     except KeyError:
-        message += "Gpu avg hz: Key not found.\n"
+        message += "Gpu avg hz: Key not found."
 
     return message
