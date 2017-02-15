86745677126734ac4b35dc4de54743336a880bce
Christian <himrasmussen@gmail.com>
2017-02-15 19:11:47 +0100

    corrected hwinfo import on webapp

--- app/webapp.py
+++ app/webapp.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
-import .hwinfo_analyze as hwinfo
-from .hwinfo_analyze import gpu, cpu, voltage, helpers
+from . import hwinfo_analyze as hwinfo
 
 UPLOAD_FOLDER = 'uploads'
 ANALYSIS_FOLDER = 'analysis'
