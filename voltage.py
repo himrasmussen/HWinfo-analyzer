eb2a83266298056bc2197023dee4d152b872c117
Christian <himrasmussen@gmail.com>
2017-02-15 10:19:20 +0100

    exception handling

--- voltage.py
+++ voltage.py
@@ -8,18 +8,28 @@ import helpers
 def main(csv_data):
     message = ''
 
-    parameter_names = None
+
     parameter = "+3.3V [V]"
-    message += helpers.check_max_parameter([parameter], csv_data, max_value=3.47)
-    message += helpers.check_min_parameter([parameter], csv_data, min_value=3.14)
+    try:
+        message += helpers.check_max_parameter([parameter], csv_data, max_value=3.47)
+        message += helpers.check_min_parameter([parameter], csv_data, min_value=3.14)
+    except KeyError:
+        message += "Key error: {}".format(parameter)
 
     parameter = "+5V [V]"
-    message += helpers.check_max_parameter([parameter], csv_data, max_value=5.25)
-    message += helpers.check_min_parameter([parameter], csv_data, min_value=4.75)
+    try:
+        message += helpers.check_max_parameter([parameter], csv_data, max_value=5.25)
+        message += helpers.check_min_parameter([parameter], csv_data, min_value=4.75)
+    except KeyError:
+        message += "Key error: {}".format(parameter)
 
     parameter = "+12V [V]"
-    message += helpers.check_max_parameter([parameter], csv_data, max_value=12.60)
-    message += helpers.check_min_parameter([parameter], csv_data, min_value=11.40)
+    try:
+        message += helpers.check_max_parameter([parameter], csv_data, max_value=12.60)
+        message += helpers.check_min_parameter([parameter], csv_data, min_value=11.40)
+    except KeyError:
+        message += "Key error: {}".format(parameter)
+
 
 
 
