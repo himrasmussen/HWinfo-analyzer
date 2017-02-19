50562de9f6885452cb8a55cecbae18bb32bf6e68
Christian <himrasmussen@gmail.com>
2017-02-19 14:46:43 +0100

    included all three images for html

--- voltage.py
+++ voltage.py
@@ -21,7 +21,7 @@ def main(csv_data):
         message += helpers.check_max_parameter(parameter, csv_data, max_value=3.47)
         message += helpers.check_min_parameter(parameter, csv_data, min_value=3.14)
         image_path = graph_maker.draw_chart(data, parameter)
-        #message += helpers.html_formatted_image(image_path)
+        message += helpers.html_formatted_image(image_path)
         #message += helpers.check_ripple(parameter, csv_data)
 
     parameter = "+5V [V]"
@@ -45,7 +45,7 @@ def main(csv_data):
         message += helpers.check_max_parameter(parameter, csv_data, max_value=12.60)
         message += helpers.check_min_parameter(parameter, csv_data, min_value=11.40)
         image_path = graph_maker.draw_chart(data, parameter)
-        #message += helpers.html_formatted_image(image_path)
+        message += helpers.html_formatted_image(image_path)
         #message += helpers.check_ripple(parameter, csv_data)
 
 
@@ -58,7 +58,7 @@ def main(csv_data):
     else:
         da_list = [_3v_relative, _5_relative, _12v_relative]
         image_path = graph_maker.draw_multiple_lines(da_list)
-        #message += helpers.html_formatted_image(image_path)
+        message += helpers.html_formatted_image(image_path)
 
 
     return helpers.sort_message(message)
